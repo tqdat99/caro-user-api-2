e49292f3f746e26b7a142e77401ec2505996576e
unknown <tqdat99@gmail.com>
2021-01-10 10:48:00 +0700

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -78,29 +78,6 @@ userSchema.pre('save', function (next) {
   }
 });
 
-userSchema.pre('save', function (next) {
-  var user = this;
-  if (!this.password) {
-    return next();
-  }
-  if (this.isModified('password') || this.isNew) {
-    bcrypt.genSalt(10, function (err, salt) {
-      if (err) {
-        return next(err);
-      }
-      bcrypt.hash(user.password, salt, function (err, hash) {
-        if (err) {
-          return next(err);
-        }
-        user.password = hash;
-        next();
-      });
-    });
-  } else {
-    return next();
-  }
-});
-
 userSchema.methods.hashPassword = function (passw, cb) {
   bcrypt.genSalt(10, function (err, salt) {
     if (err) {
